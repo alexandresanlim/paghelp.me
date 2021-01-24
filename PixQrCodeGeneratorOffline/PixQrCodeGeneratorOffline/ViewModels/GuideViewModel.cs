212bcd64db6a415d0a9426ea313f520f6c79537f
Alexandre Lima <alexandreslima@outlook.com>
2021-01-24 20:05:42 -0300

    add guide

---
+++ PixQrCodeGeneratorOffline/PixQrCodeGeneratorOffline/ViewModels/GuideViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixQrCodeGeneratorOffline.ViewModels
+{
+    public class GuideViewModel : BaseViewModel
+    {
+    }
+}
---
+++ PixQrCodeGeneratorOffline/PixQrCodeGeneratorOffline/Views/GuidePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using PixQrCodeGeneratorOffline.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PixQrCodeGeneratorOffline.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GuidePage : ContentPage
+    {
+        GuideViewModel _guideViewModel;
+
+        public GuidePage()
+        {
+            InitializeComponent();
+
+            BindingContext = _guideViewModel = new GuideViewModel();
+        }
+    }
+}
