27702db659b74af9b4b4e955f7e4c0f25bad00cf
Alexandre Lima <alexandreslima@outlook.com>
2021-03-13 20:21:09 -0300

    improvements show in list

---
+++ PixQrCodeGeneratorOffline/PixQrCodeGeneratorOffline/Models/Guide.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixQrCodeGeneratorOffline.Models
+{
+    public class Guide
+    {
+        public Guide(string _question, string _answer)
+        {
+            Question = _question;
+            Answer = _answer;
+        }
+
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
