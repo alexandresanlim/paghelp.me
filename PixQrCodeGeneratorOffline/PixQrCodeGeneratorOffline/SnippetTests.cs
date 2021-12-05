9b96f2412be152481a1d060d0dbf3072eb76c095
Alexandre Lima <alexandreslima@outlook.com>
2021-12-05 15:35:02 -0300

    varsao 0.1.12

---
+++ PixQrCodeGeneratorOffline/PixQrCodeGeneratorOffline/SnippetTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PixQrCodeGeneratorOffline
+{
+    public class SnippetTests : Frame
+    {
+
+        public static readonly BindableProperty MyPropertyProperty =
+           BindableProperty.Create(
+               propertyName: nameof(MyProperty),
+               returnType: typeof(string),
+               declaringType: typeof(object),
+               defaultValue: string.Empty,
+               defaultBindingMode: BindingMode.Default,
+               validateValue: null,
+               propertyChanged: MyPropertyPropertyChanged);
+
+        public string MyProperty
+        {
+            get => (string)GetValue(MyPropertyProperty);
+            set => SetValue(MyPropertyProperty, value);
+        }
+
+        static void MyPropertyPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (newValue != null)
+                return;
+
+            var template = (object)bindable;
+            string value = (string)newValue;
+        }
+    }
+}
